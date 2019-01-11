fea86998ffcb8cd8dbb9a9fb7b9086b45193fd8e
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2019-01-11 21:15:16 +0800

    delete js files

--- src/ticker/model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Ticker = /** @class */ (function () {
-    function Ticker(name) {
-        this.name = name;
-    }
-    return Ticker;
-}());
-exports.Ticker = Ticker;
