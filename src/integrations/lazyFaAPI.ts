d56cf485a34b97ae1b197c09170834f3719793bc
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2019-11-09 13:40:51 +0800

    minor updates

--- src/integrations/lazyFaAPI.ts
+++ src/integrations/lazyFaAPI.ts
@@ -2,17 +2,18 @@ import Axios from "axios";
 import _ from "lodash";
 
 export default class lazyFaAPI {
+  private static tickerList = null;
 
-    private static tickerList = null;
+  static async getTickerList() {
+    if (this.tickerList) return this.tickerList;
 
-    static async getTickerList() {
-        if (this.tickerList) return this.tickerList;
+    const tickerListResult = await Axios.get(
+      "https://s3.amazonaws.com/lfatickerlists/sf1-tickers.json"
+    );
+    const tickerList = _.get(tickerListResult, "data");
 
-        const tickerListResult = await Axios.get("https://s3.amazonaws.com/lfatickerlists/sf1-tickers.json");
-        const tickerList = _.get(tickerListResult, "data");
+    this.tickerList = tickerList;
 
-        this.tickerList = tickerList;
-        
-        return tickerList;
-    }
+    return tickerList;
+  }
 }
