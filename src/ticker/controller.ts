6ea3555629b3d278ed73a3fcf7edc5f2f3f9766c
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2019-11-09 13:42:30 +0800

    fix lazyfaapi caps typo

--- src/ticker/controller.ts
+++ src/ticker/controller.ts
@@ -2,34 +2,30 @@ import financialModelingPrepAPI from "../integrations/financialModelingPrepAPI";
 import Ticker from "./Ticker";
 import Axios from "axios";
 import * as _ from "lodash";
-import lazyFaAPI from "../integrations/lazyFAAPI";
+import lazyFaAPI from "../integrations/lazyFaApi";
 
 export default class TickerController {
+  private static cache: { [s: string]: Ticker } = {};
+  private static tickerList: { [Exchange: string]: string }[] = [];
 
-    private static cache: {[s:string]: Ticker} = {};
-    private static tickerList: {[Exchange: string]: string, }[] = [];
+  public static async get(tickerName: string) {
+    // let ticker = this.cache[tickerName];
+    // if (!ticker){
+    // ticker = await this.initTicker(tickerName);
+    // this.cache[tickerName] = ticker;
+    // }
+    const ticker = await this.initTicker(tickerName);
+    return ticker;
+  }
 
-    public static async get(tickerName: string) {
-        // let ticker = this.cache[tickerName];
-        // if (!ticker){
-            // ticker = await this.initTicker(tickerName);
-            // this.cache[tickerName] = ticker;
-        // }
-        const ticker = await this.initTicker(tickerName);
-        return ticker;
-    }
+  public static async getList() {
+    return lazyFaAPI.getTickerList();
+  }
 
-    public static async getList(){
-        return lazyFaAPI.getTickerList();
-    }
-
-    private static async initTicker(tickerName: string): Promise<Ticker>{
-
-
-        const tickerData = await financialModelingPrepAPI.getAllTickerFinanceData(tickerName)
-        return new Ticker(tickerName, tickerData);
-    }
-
-
-
+  private static async initTicker(tickerName: string): Promise<Ticker> {
+    const tickerData = await financialModelingPrepAPI.getAllTickerFinanceData(
+      tickerName
+    );
+    return new Ticker(tickerName, tickerData);
+  }
 }
